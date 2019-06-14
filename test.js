d19d526a2db1a23789c20c454fc4911b38e9ecce
David <david@outlier.org>
2019-06-14 13:26:16 -0700

    feat: add user-agent endpoint

--- test.js
+++ test.js
@@ -23,6 +23,15 @@ tape('should respond b64', (t) => {
     t.end()
   })
 })
+tape('should respond user-agent', (t) => {
+  const opts = { headers: { 'User-Agent': 'tape' } }
+  jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.ua, 'tape')
+    t.end()
+  })
+})
 
 tape('cleanup', function (t) {
   server.close()
